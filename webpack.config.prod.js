69160d7c6704ccf1915bb168a4dcfc579c031107
mohammaadreza berneti <mberneti@live.com>
2017-03-26 02:43:30 +0430

    added UglifyJsPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,5 +18,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin()]
 }
