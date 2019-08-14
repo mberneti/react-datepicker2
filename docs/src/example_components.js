104e8c9f96891a144f14dad987938b4bc427060b
Mohammadreza Berneti <mberneti@live.com>
2019-08-14 14:28:00 +0430

    added a sample of removeDate usage, close #32

--- docs/src/example_components.js
+++ docs/src/example_components.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import hljs from 'highlight.js'
+import hljs from 'highlight.js';
 import CodeExampleComponent from './code_example_component';
 import Highlight from 'react-syntax-highlight';
 
@@ -13,75 +13,75 @@ import DisabledRange from './examples/disabledRange';
 import HighlightRange from './examples/HighlightRange';
 import Disabled from './examples/Disabled';
 import ReactHook from './examples/ReactHook';
+import RemoveDate from './examples/RemoveDate';
 
 import { StickyContainer, Sticky } from 'react-sticky';
-
 
 export default class ExampleComponents extends React.Component {
   constructor(props) {
     super(props);
     this.examples = [
       Default,
-      Jalaali, DisabledTimepicker,
-      SwitchIsGregorian, CalendarPicker,
-      LimitWithMinMax, 
-      DisabledRange, 
-      HighlightRange, Disabled, ReactHook];
+      Jalaali,
+      DisabledTimepicker,
+      SwitchIsGregorian,
+      CalendarPicker,
+      LimitWithMinMax,
+      DisabledRange,
+      HighlightRange,
+      Disabled,
+      ReactHook,
+      RemoveDate
+    ];
   }
 
   componentDidMount() {
-    hljs.initHighlightingOnLoad()
+    hljs.initHighlightingOnLoad();
   }
 
   renderExamples() {
-    return this.examples.map((example, index) =>
+    return this.examples.map((example, index) => (
       <CodeExampleComponent key={`example-${index}`} id={index} title={example.title}>
         <div className="row">
           <div className="column">
             <Highlight lang="jsx" value={example.code} />
           </div>
-          <div className="column">
-            {<example.component />}
-          </div>
+          <div className="column">{<example.component />}</div>
         </div>
       </CodeExampleComponent>
-    )
+    ));
   }
 
   renderLeftColumn() {
-    return this.examples.map((example, index) =>
+    return this.examples.map((example, index) => (
       <li className="examples__navigation-item" key={`link-${index}`}>
-        <a href={`#example-${index}`}>
-          {example.title}
-        </a>
+        <a href={`#example-${index}`}>{example.title}</a>
       </li>
-    )
+    ));
   }
 
   render() {
-    return ( 
-        <div> 
+    return (
+      <div>
         <StickyContainer>
           <Sticky>
-          {({ style,isSticky }) => {
-              const border = isSticky?{border:'none'}:{};
-              return <h1 style={{...style,...border}}>Examples</h1>
+            {({ style, isSticky }) => {
+              const border = isSticky ? { border: 'none' } : {};
+              return <h1 style={{ ...style, ...border }}>Examples</h1>;
             }}
           </Sticky>
           <div className="examples__navigation-container">
-          <Sticky>
-          {({ style }) => (
+            <Sticky>
+              {({ style }) => (
                 <ul style={style} className="examples__navigation">
                   {this.renderLeftColumn()}
                 </ul>
-            )}
-          </Sticky>
-            </div>
-              <div className="examples">
-                {this.renderExamples()}
-              </div>
-      </StickyContainer>
-        </div>
+              )}
+            </Sticky>
+          </div>
+          <div className="examples">{this.renderExamples()}</div>
+        </StickyContainer>
+      </div>
     );
   }
 }
---
+++ docs/src/examples/RemoveDate.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import moment from 'moment-jalaali';
+import DatePicker from '../../../src/components/DatePicker';
+
+class component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: moment('1396/7/6', 'jYYYY/jM/jD')
+    };
+  }
+  render() {
+    return (
+      <div>
+        <DatePicker
+          ref={r => {
+            this.dt = r;
+          }}
+          value={this.state.value}
+          onChange={value => this.setState({ value })}
+        />
+        <br />
+        <button onClick={() => this.dt.removeDate()}>remove date</button>
+      </div>
+    );
+  }
+}
+
+const title = 'Remove Date';
+const code = `class component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: moment('1396/7/6', 'jYYYY/jM/jD')
+    };
+  }
+  render() {
+    return (
+      <div>
+        <DatePicker
+          ref={r => {
+            this.dt = r;
+          }}
+          value={this.state.value}
+          onChange={value => this.setState({ value })}
+        />
+        <br />
+        <button onClick={() => this.dt.removeDate()}>remove date</button>
+      </div>
+    );
+  }
+} }
+}
+`;
+const Disabled = { component, title, code };
+export default Disabled;
