e3e8e6d33b462316b1de230fdae78a86ca7252bc
Mohammadreza Berneti <mberneti@live.com>
2019-12-10 00:47:52 +0330

    - upgraded rollup package - fixed rollup ' is not exported' bug

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import postcss from 'rollup-plugin-postcss';
 
 const globals = {
   react: 'React',
-  'react-dom': 'ReactDOM',
+  'react-dom': 'ReactDOM'
 };
 
 const config = {
@@ -14,21 +14,29 @@ const config = {
   output: {
     format: 'commonjs',
     name: 'react-datepicker2',
-    globals,
+    globals
   },
   plugins: [
     postcss({
       plugins: []
     }),
     nodeResolve({
-      extensions: [".js", ".jsx"]
+      extensions: ['.js', '.jsx']
     }),
     babel({
       exclude: 'node_modules/**',
       runtimeHelpers: true
     }),
-    commonjs(),
-    nodeGlobals(),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+
+        'node_modules/rc-animate/node_modules/fbjs/lib/ExecutionEnvironment.js': ['canUseDOM']
+      }
+    }),
+    nodeGlobals()
   ],
   external: Object.keys(globals)
 };
